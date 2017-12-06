2dfe7212726ed7eb109b0ffcf1bcc6b749b03478
h22turbo <32185056+WarezAddict-com@users.noreply.github.com>
2017-12-06 01:10:10 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,39 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 let cleanCSS = require('gulp-clean-css');
-
-gulp.task('minify-js', function() {
-  return gulp.src('js/custom.js')
-    .pipe(uglify())
-    .pipe(rename({ extname: '.min.js' }))
-    .pipe(gulp.dest('js/'));
-});
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('minify-css', () => {
   return gulp.src('css/custom.css')
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+    }))
     .pipe(cleanCSS({debug: true}, (details) => {
       console.log(`${details.name}: ${details.stats.originalSize}`);
       console.log(`${details.name}: ${details.stats.minifiedSize}`);
     }))
   .pipe(rename({ extname: '.min.css' }))
-  .pipe(gulp.dest('css'));
+  .pipe(gulp.dest('assets/css/'));
 });
 
-gulp.task('default', ['minify-js', 'minify-css'])
+gulp.task('minify-js', function() {
+  return gulp.src('js/custom.js')
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('assets/js/'));
+});
+
+gulp.task('opti-images', function() {
+    return gulp.src('images/*')
+      .pipe(imagemin({
+        interlaced: true,
+        progressive: true,
+        optimizationLevel: 8, // 5 saves 22.9 kB or 18.2%
+        svgoPlugins: [{removeViewBox: false}, {removeUselessStrokeAndFill:false}]
+      }))
+      .pipe(gulp.dest('assets/images/'))
+});
+
+gulp.task('default', ['minify-css', 'minify-js', 'opti-images'])
