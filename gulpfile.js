e05e18119578b4e3f21f3b3d4db3c23809c6730f
h22turbo <32185056+WarezAddict-com@users.noreply.github.com>
2017-12-05 23:44:46 -0500

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+let cleanCSS = require('gulp-clean-css');
+
+gulp.task('minify-js', function() {
+  return gulp.src('js/custom.js')
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('js/'));
+});
+
+gulp.task('minify-css', () => {
+  return gulp.src('css/custom.css')
+    .pipe(cleanCSS({debug: true}, (details) => {
+      console.log(`${details.name}: ${details.stats.originalSize}`);
+      console.log(`${details.name}: ${details.stats.minifiedSize}`);
+    }))
+  .pipe(rename({ extname: '.min.css' }))
+  .pipe(gulp.dest('css'));
+});
